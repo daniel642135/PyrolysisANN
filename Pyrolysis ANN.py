bc353e19e39fda2b962faaf55d047b4b42174406
daniel642135 <daniel642135@hotmail.com>
2020-01-31 19:55:25 +0800

    updated model to attempt print the test result and include early stopping

--- Pyrolysis ANN.py
+++ Pyrolysis ANN.py
@@ -1,7 +1,12 @@
-import tensorflow as tf
-import numpy as np
+# import tensorflow as tf
+# import math
+# import numpy as np
+# from tensorflow import keras
+# import matplotlib.pyplot as plt
 from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.callbacks import EarlyStopping
+
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -12,19 +17,37 @@ X = dataset.drop(labels=['Liquid', 'Gas', 'Tar'], axis = 1)
 y = dataset.drop(labels=['HDPE', 'LDPE', 'PP', 'PS', 'Tar'], axis = 1)
 X.head()
 y.head()
+# Dimensions of dataset
+n = X.shape[0]
+p = X.shape[1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
 
-model = Sequential()
-model.add(Dense(X.shape[1], activation='relu', input_dim = X.shape[1]))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(y.shape[1], activation ='sigmoid'))
-model.compile(optimizer='adam', loss = 'mse', metrics=['accuracy'])
-model.fit(X_train, y_train, epochs = 10, verbose = 1)
-y_pred = model.predict(X_test)
-model.evaluate(X_test, y_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+# Scale data
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
 print(X_test)
-print(y_pred)
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
+def create_model():
+    model = Sequential()
+    model.add(Dense(X.shape[1], activation='sigmoid', input_dim = X.shape[1]))
+    model.add(Dense(5, activation='sigmoid'))
+    model.add(Dense(y.shape[1], activation ='sigmoid'))
+    model.compile(optimizer='SGD', loss = 'mse', metrics=['accuracy'])
+    return model
 
+model = create_model()
+model.summary()
+earlystop_callback = EarlyStopping(
+  monitor='accuracy', min_delta=0.0001,
+  patience=1)
+model.fit(X_train, y_train.to_numpy(), epochs = 100, verbose = 1, callbacks=[earlystop_callback])
+model.save_weights('./checkpoints/my_checkpoints')
+y_pred = model.predict(X_test)
 
+model = create_model()
+# model.load_weights('./checkpoints/my_checkpoints')
+loss, accuracy = model.evaluate(X_test, y_test.to_numpy())
+print("Accuracy = {:5.2f}%".format(100*accuracy))
